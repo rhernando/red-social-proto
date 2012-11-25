b69f993907d3980c5440397cd17b6c96b00b5dde
Ruben Hernando <hernando.ruben@gmail.com>
2012-11-25 16:28:55 +0100

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedSocialProto::Application.config.secret_token = '9585a2bee5a363c134ad7008f5ccd02320abc9a19cfd173939907bb533eeb48c7cdb6551bb7a014355b1b4d3517290da9ea9f59bb571a11d273084de12ff42ec'
